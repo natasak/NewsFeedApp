88e423d82276b8146ac04d080950a782cb3ca473
Natasa Mori <natasa@the-mori.com>
2018-07-19 23:23:22 +0200

    Added "order by" preference.

--- app/src/main/java/com/example/natasa/newsfeedapp/SettingsActivity.java
+++ app/src/main/java/com/example/natasa/newsfeedapp/SettingsActivity.java
@@ -17,6 +17,11 @@ public class SettingsActivity extends AppCompatActivity {
      */
     public static class NewsPreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
 
